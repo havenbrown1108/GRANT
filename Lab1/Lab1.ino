0a77a32cae71a5864664875e5620c9a44d289123
Anirudh <anirudh.tangellapalli@gmail.com>
2024-02-05 13:38:34 -0600

    Updated lab 1 with comments

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,129 +1,78 @@
 /* 
-
-Ringo Robot
-Ringo_Base_Sketch_Rev06
+Lab1
+Play songs and blinks 
 Version 6.2 03/2018
-
-This is a basic sketch that can be used as a starting point
-for various functionality of the Ringo robot.
-
-Significant portions of this code written by
-Dustin Soodak for Plum Geek LLC. Some portions
-contributed by Kevin King.
-Portions from other open source projects where noted.
-This code is licensed under:
-Creative Commons Attribution-ShareAlike 2.0 Generic (CC BY-SA 2.0)
-https://creativecommons.org/licenses/by-sa/2.0/
-Visit http://www.plumgeek.com for Ringo information.
-Visit http://www.arduino.cc to learn about the Arduino.
-
 */
 #include <Arduino_FreeRTOS.h>
 #include "RingoHardware.h"
 
-//int i;    //declaire any global variables here
-
-void TaskBlinkRandomly( void *pvParameters );
 void TaskSing( void *pvParameters );
 void playNote(int note);
 void restNote(int restLength);
 
-int noteLength = 150;
-int volume = 40;
+// Globals
+int noteLength = 150; // controls note length and will effect the speed of songs
+int volume = 40; // Controls amplitude/volume of notes played
 
 
 void setup(){
   HardwareBegin();        //initialize Ringo's brain to work with his circuitry
   SwitchButtonToPixels(); //set shared line to control NeoPixel lights
-  // PlayStartChirp();       //Play startup chirp and blink eyes
   SwitchMotorsToSerial(); //Call "SwitchMotorsToSerial()" before using Serial.print functions as motors & serial share a line
-  RxIRRestart(4);         //wait for 4 byte IR remote command
+  // Set up for IR
+  RxIRRestart(4);
   IsIRDone();
   GetIRButton();
   RestartTimer();
 
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
-  }
-
-  // Now set up two tasks to run independently.
-  // xTaskCreate(
-  // TaskBlinkRandomly
-  // ,  (const portCHAR *)"Blink"   // A name just for humans
-  // ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
-  // ,  NULL
-  // ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-  // ,  NULL );
-
   xTaskCreate(
   TaskSing
-  ,  (const portCHAR *)"Sing"   // A name just for humans
-  ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+  ,  (const portCHAR *)"Sing"
+  ,  128 
   ,  NULL
-  ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+  ,  2 
   ,  NULL );
-
-
-
 }
 
 void loop(){}
 
-void TaskBlinkRandomly(void *pvParameters)  // This is a task.
+void TaskSing(void *pvParameters) 
 {
-  (void) pvParameters;
-  for (;;) // A Task shall never return or exit.
-  {
-    RandomEyes();
-    vTaskDelay( 500 / portTICK_PERIOD_MS ); 
-    OffEyes();
-    vTaskDelay( 500 / portTICK_PERIOD_MS ); 
-  }
-}
-
-void TaskSing(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
   byte button;
-  for (;;) // A Task shall never return or exit.
+  for (;;) 
   {
+    // While idle and waiting for input blink eyes randomly
     RandomEyes();
     vTaskDelay( 500 / portTICK_PERIOD_MS ); 
     OffEyes();
 
+    // Check for input
     if (IsIRDone()) {
       button = GetIRButton();
       if (button) {
         switch (button) {
-          case 1:
+          case 1: // If button 1 is pressed then play Megalovania
           OffEyes();
           PlayMegalovania();
           RxIRRestart(4);
           break;
-
-          case 2:
+          case 2: // If button 2 is pressed then play Mii Music
           OffEyes();
           PlayMiiSong();
           RxIRRestart(4);
           break;
-
           default:
           RxIRRestart(4);
           break;
         }
       }
-
     }
-    
     RxIRRestart(4);
-    vTaskDelay( 500 / portTICK_PERIOD_MS );
+    vTaskDelay( 500 / portTICK_PERIOD_MS ); // Check every 0.5s
   }
 }
 
-// Put source of sheet music
+// from https://pianoletternotes.blogspot.com/2017/10/megalovania-undertale-theme.html
 void PlayMegalovania() {
   SetAllPixelsRGB(100,0,0);
 
@@ -441,7 +390,7 @@ void MegSeventhStanza(){
   restNote(1);
 }
 
-// Put source of sheet music
+// from https://pianoletternotes.blogspot.com/2017/11/mii-channel-theme.html
 void PlayMiiSong() {
   SetAllPixelsRGB(0,100,0);
 
