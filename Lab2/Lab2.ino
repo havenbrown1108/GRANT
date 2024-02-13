e8385a11199ac2ac8f6792d7a30f80e21d047562
Haven Brown <hbrown28@unl.edu>
2024-02-13 17:27:20 -0600

    filled in two periodic tasks and one aperiodic task. the aperiodic task could use some work

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -6,13 +6,14 @@ Version 6.2 03/2018
 #include <Arduino_FreeRTOS.h>
 #include "RingoHardware.h"
 
-void TaskSing( void *pvParameters );
-void playNote(int note);
-void restNote(int restLength);
+void TaskPeriodicSound(void *pvParameters);
+void TaskPeriodicLights(void *pvParameters);
+void TaskAperiodic(void *pvParameters);
 
 // Globals
 int noteLength = 150; // controls note length and will effect the speed of songs
 int volume = 40; // Controls amplitude/volume of notes played
+int eyeColor[3] = {255, 0, 0};
 
 
 void setup(){
@@ -23,77 +24,95 @@ void setup(){
   RxIRRestart(4);
   IsIRDone();
   GetIRButton();
-  RestartTimer();
+  // RestartTimer();
 
   xTaskCreate(
-  TaskSing
-  ,  (const portCHAR *)"Sing"
+  TaskPeriodicSound
+  ,  (const portCHAR *)"Beep"
+  ,  128 
+  ,  NULL
+  ,  1 
+  ,  NULL );
+
+  xTaskCreate(
+  TaskPeriodicLights
+  ,  (const portCHAR *)"Blink"
   ,  128 
   ,  NULL
   ,  2 
+  ,  NULL );
+
+  xTaskCreate(
+  TaskAperiodic
+  ,  (const portCHAR *)"Change Colors"
+  ,  128 
+  ,  NULL
+  ,  3 
   ,  NULL );
 }
 
 void loop(){}
 
-void TaskSing(void *pvParameters) 
-{
-  byte button;
-  for (;;) 
-  {
-    // While idle and waiting for input blink eyes randomly
-    RandomEyes();
-    vTaskDelay( 500 / portTICK_PERIOD_MS ); 
-    OffEyes();
 
-    // Check for input
-    if (IsIRDone()) {
-      button = GetIRButton();
-      if (button) {
-        switch (button) {
-          case 1: // If button 1 is pressed then play Megalovania
-          OffEyes();
-          PlayMegalovania();
-          RxIRRestart(4);
-          break;
-          case 2: // If button 2 is pressed then play Mii Music
-          OffEyes();
-          PlayMiiSong();
-          RxIRRestart(4);
-          break;
-          default:
-          RxIRRestart(4);
-          break;
-        }
-      }
-    }
-    RxIRRestart(4);
-    vTaskDelay( 500 / portTICK_PERIOD_MS ); // Check every 0.5s
+void TaskPeriodicSound(void *pvParameters)
+{
+  TickType_t xLastWakeTime;
+  xLastWakeTime = xTaskGetTickCount();
+
+  for(;;)
+  {
+    PlayChirp(NOTE_D4, volume);
+    vTaskDelayUntil( &xLastWakeTime, noteLength / portTICK_PERIOD_MS );
+    // vTaskDelay(10000 / portTICK_PERIOD_MS);
+    OffChirp();
+    // vTaskDelay(10000 / portTICK_PERIOD_MS);
+    vTaskDelayUntil( &xLastWakeTime, noteLength / portTICK_PERIOD_MS );
   }
 }
 
-void TaskPeriodic(void *pvParameters)
+void TaskPeriodicLights(void *pvParameters)
 {
-  PlayChirp(note, volume);
-  vTaskDelay(noteLength / portTICK_PERIOD_MS);
-  OffChirp();
-  vTaskDelay( noteLength * restLength/ portTICK_PERIOD_MS );
+  TickType_t xLastWakeTime;
+  xLastWakeTime = xTaskGetTickCount();
+
+  for(;;)
+  {
+    SetPixelRGB(EYE_LEFT,eyeColor[0],eyeColor[1],eyeColor[2]);
+    SetPixelRGB(EYE_RIGHT,eyeColor[0],eyeColor[1],eyeColor[2]);
+    vTaskDelayUntil( &xLastWakeTime, 500 / portTICK_PERIOD_MS);
+    OffEyes();
+    vTaskDelayUntil( &xLastWakeTime, 500 / portTICK_PERIOD_MS);
+  }
 }
 
-void TaskAperiodic(void xpvParameters)
+void TaskAperiodic(void *pvParameters)
 {
-  if (IsIRDone()) {
+  byte button;
+  for(;;)
+  {
+    // if (IsIRDone()) {
       button = GetIRButton();
       if (button) {
         switch (button) {
-          case 1: // If button 1 is pressed then play Megalovania
-          OffEyes();
-          PlayMegalovania();
+          case 1: // If button 1 is pressed change eyes to red
+          eyeColor[0] = 255;
+          eyeColor[1] = 0;
+          eyeColor[2] = 0;
+          // OnEyes(255, 0, 0);
           RxIRRestart(4);
           break;
-          case 2: // If button 2 is pressed then play Mii Music
-          OffEyes();
-          PlayMiiSong();
+          case 2: // If button 2 is pressed change eyes to green
+          eyeColor[0] = 0;
+          eyeColor[1] = 255;
+          eyeColor[2] = 0;
+          // OnEyes(0, 255, 0);
+          RxIRRestart(4);
+          break;
+          case 3: // If button 3 is pressed change eyes to blue
+          eyeColor[0] = 0;
+          eyeColor[1] = 0;
+          eyeColor[2] = 255;
+          // OnEyes(0, 0, 255);
           RxIRRestart(4);
           break;
           default:
@@ -101,6 +120,10 @@ void TaskAperiodic(void xpvParameters)
           break;
         }
       }
-    }
+    // }
+    
     RxIRRestart(4);
+    vTaskDelay( 100 / portTICK_PERIOD_MS ); 
+  }
+  
 }
